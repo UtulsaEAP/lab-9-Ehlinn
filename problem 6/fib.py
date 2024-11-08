ca3f1248cce5d7bbac676f7e4a5960810aea4fb5
Hunter Linn <ehl1436@utulsa.edu>
2024-11-08 17:49:08 -0600

    Good through 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,8 +1,18 @@
 def fibonacci(n):
-    
-    #write your code here
-    return
-
+    fibnum = 1
+    fibtwo = 0
+    fibval = 0
+    if n < 0:
+        fibval = -1
+    elif n == 1:
+        fibval = 1
+    while n > 1:
+        fibprev = fibnum
+        fibnum = fibnum + fibtwo
+        fibval = fibnum
+        fibtwo = fibprev
+        n += -1
+    return fibval
 if __name__ == '__main__':
     start_num = int(input())
     print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
