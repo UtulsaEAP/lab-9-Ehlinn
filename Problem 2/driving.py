ca03d862034e9e820dfe5d75eb3003abe46bffe1
Hunter Linn <ehl1436@utulsa.edu>
2024-11-08 17:27:17 -0600

    Good through 5

--- Problem 2/driving.py
+++ Problem 2/driving.py
@@ -9,4 +9,3 @@ if __name__ == '__main__':
     print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
     print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 50.0):.2f}')
     print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 400.0):.2f}')
-   
--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,5 +1,6 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
-   #write your code here
+   
+   return(user_val2, user_val1, user_val4, user_val3)
    
    return user_val1, user_val2, user_val3, user_val4
 
@@ -8,7 +9,9 @@ if __name__ == '__main__':
    user_input2 = int(input())
    user_input3 = int(input())
    user_input4 = int(input())
-   #store output for every input here
-   #print those output
 
- 
+   stringrev = str(swap_values(user_input1, user_input2, user_input3, user_input4))
+   
+   print(stringrev)
+   #store output for every input here
+   #print those outputs
